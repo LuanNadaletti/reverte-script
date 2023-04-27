37d7c5055ff41b646e35f1d56bfeedcb57239b4c
pvL666 <luannadaletti6@gmail.com>
2023-04-27 20:30:59 -0300

    :vertical_traffic_light: adicionado testes unitários para InsertQuery no parse

--- src/test/parsers/QueryParserTest.java
+++ src/test/parsers/QueryParserTest.java
@@ -1,5 +1,28 @@
 package test.parsers;
 
-public class QueryParserTest {
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import main.models.InsertQuery;
+import main.models.Query;
+import main.parsers.QueryParser;
+
+class QueryParserTest {
+    
+    @Test
+    @DisplayName("InsertQueryParse")
+    void testInsertQueryParse() {
+    	String script = "INSERT INTO GER_MENU (MNU_COD, MNU_DESC, MNU_NIVEL) VALUES (1, 'DESC', 'NIV')";
+    	QueryParser parser = new QueryParser(script);
+    	Query query = parser.parse().get(0);
+    	
+    	assertTrue(query instanceof InsertQuery);
+    	InsertQuery selectQuery = (InsertQuery) query;
+    	
+    	assertEquals("GER_MENU", selectQuery.getTable());
+    	assertEquals(Arrays.asList("MNU_COD", "MNU_DESC", "MNU_NIVEL") ,selectQuery.getFields());
+    	assertEquals(Arrays.asList("1", "'DESC'", "'NIV'"), selectQuery.getValues());
+    }
 
 }
