d4061bffc8f29b99afd3d2dca7d02eedd867cfc1
Luan <luannadaletti6@gmail.com>
2023-06-06 14:39:30 -0300

    :memo: adicionado Javadoc de toda a classe

--- src/main/parsers/QueryParser.java
+++ src/main/parsers/QueryParser.java
@@ -7,69 +7,101 @@ import main.factories.QueryFactory;
 import main.models.Query;
 
 /**
+ * The QueryParser class is responsible for parsing a given SQL script and
+ * returning a list of all SQL statements found in the script.
+ *
+ * <p>
+ * This class uses a set of methods to perform the analysis and extraction of
+ * SQL statements. The SQL script is provided through the constructor, and the
+ * {@link #parse()} method is used to initiate the parsing.
+ * </p>
+ *
+ * *
+ * <p>
+ * Usage example:
+ *
+ * <pre>{@code
+ * String script = "INSERT INTO table (column) VALUES ('value');";
+ * QueryParser parser = new QueryParser(script);
+ * List<Query> queries = parser.parse();
+ * }</pre>
+ * </p>
  *
  * @author Luan Nadaletti
- *
+ * @see Query
  */
 public class QueryParser {
 
-    private String script;
-    private LinkedList<Query> queries = new LinkedList<>();
+	private String script;
+	private LinkedList<Query> queries = new LinkedList<>();
 
-    private int currentIndex = 0;
-    private boolean insideString = false;
+	private int currentIndex = 0;
+	private boolean insideString = false;
 
-    public QueryParser(String script) {
-        this.script = script;
-    }
+	public QueryParser(String script) {
+		this.script = script;
+	}
 
-    /**
-     *
-     * Analisa o script SQL fornecido e retorna uma lista de todas as declarações SQL encontradas.
-     *
-     * @return Uma lista de todas as declarações SQL encontradas no script.
-     */
-    public List<Query> parse() {
-        StringBuilder currentStatement = new StringBuilder();
+	/**
+	 * Parses the provided SQL script and returns a list of all SQL statements
+	 * found.
+	 *
+	 * @return A list of all SQL statements found in the script.
+	 */
+	public List<Query> parse() {
+		StringBuilder currentStatement = new StringBuilder();
 
-        while (currentIndex < script.length()) {
-            char character = getNextCharacter();
+		while (currentIndex < script.length()) {
+			char character = getNextCharacter();
 
-            if (character == '\'') {
-                handleStringDelimiter();
-            }
+			if (character == '\'') {
+				handleStringDelimiter();
+			}
 
-            if (!insideString && character == ';') {
-                currentStatement.append(character);
-                addQuery(currentStatement.toString());
-                currentStatement = new StringBuilder();
-            } else {
-                currentStatement.append(character);
-            }
-        }
+			if (!insideString && character == ';') {
+				currentStatement.append(character);
+				addQuery(currentStatement.toString());
+				currentStatement = new StringBuilder();
+			} else {
+				currentStatement.append(character);
+			}
+		}
 
-        if (currentStatement.length() > 0) {
-            addQuery(currentStatement.toString());
-        }
+		if (currentStatement.length() > 0) {
+			addQuery(currentStatement.toString());
+		}
 
-        return queries;
-    }
+		return queries;
+	}
 
-    private void addQuery(String statement) {
-        QueryFactory factory = QueryType.fromStatement(statement).getQueryFactory();
-        if (factory == null) {
-            return;
-        }
+	/**
+	 * Adds a new query to the list of queries.
+	 *
+	 * @param statement The SQL statement to be added.
+	 */
+	private void addQuery(String statement) {
+		QueryFactory factory = QueryType.fromStatement(statement).getQueryFactory();
+		if (factory == null) {
+			return;
+		}
 
-        queries.add(factory.createQuery(statement));
-    }
+		queries.add(factory.createQuery(statement));
+	}
 
-    private char getNextCharacter() {
-        return script.charAt(currentIndex++);
-    }
+	/**
+	 * Retrieves the next character from the SQL script.
+	 *
+	 * @return The next character from the script.
+	 */
+	private char getNextCharacter() {
+		return script.charAt(currentIndex++);
+	}
 
-    private void handleStringDelimiter() {
-        insideString = !insideString;
-    }
+	/**
+	 * Handles the string delimiter ('), toggling the inside/outside string state.
+	 */
+	private void handleStringDelimiter() {
+		insideString = !insideString;
+	}
 
 }
