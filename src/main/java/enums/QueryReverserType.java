583de0cd0b5146dbbba863cbc72ba6f4d05790a5
Luan <luannadaletti6@gmail.com>
2023-06-09 16:50:39 -0300

    :memo: adicionado javadoc

--- src/main/java/enums/QueryReverserType.java
+++ src/main/java/enums/QueryReverserType.java
@@ -7,9 +7,29 @@ import reversers.InsertQueryReverser;
 import reversers.QueryReverser;
 
 /**
+ * The {@code QueryReverserType} enum represents different types of query
+ * reversers. Each query reverser type is associated with a corresponding
+ * {@link QueryReverser} implementation that can reverse a specific type of SQL
+ * query.
+ *
+ * <p>
+ * This enum provides methods to retrieve the appropriate {@link QueryReverser}
+ * for each query reverser type, and to determine the query reverser type based
+ * on a given {@link Query} object.
+ * </p>
+ *
+ * <p>
+ * Example usage:
+ *
+ * <pre>{@code
+ * Query query = new InsertQuery("INSERT INTO table (column) VALUES ('value')");
+ * QueryReverserType reverserType = QueryReverserType.fromQuery(query);
+ * QueryReverser reverser = reverserType.getQueryReverser();
+ * }</pre>
+ * </p>
  *
  * @author Luan Nadaletti
- *
+ * @see QueryReverser
  */
 public enum QueryReverserType {
 	INSERT_REVERSER {
@@ -36,8 +56,21 @@ public enum QueryReverserType {
 
 	};
 
+	/**
+	 * Returns the {@link QueryReverser} associated with this query reverser type.
+	 *
+	 * @return The query reverser for this query reverser type.
+	 */
 	public abstract QueryReverser getQueryReverser();
 
+	/**
+	 * Determines the {@code QueryReverserType} based on the given {@link Query}
+	 * object.
+	 *
+	 * @param query The query object.
+	 * @return The query reverser type associated with the query, or {@code null} if
+	 *         no match is found.
+	 */
 	public static QueryReverserType fromQuery(Query query) {
 		if (query.toString().contains("INSERT INTO") && query.toString().contains("VALUES")) {
 			return INSERT_REVERSER;
