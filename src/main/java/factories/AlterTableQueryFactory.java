73a8febdba75f49cbce2fe363a2c13624cb52b19
Luan <luannadaletti6@gmail.com>
2023-06-20 17:30:28 -0300

    :adhesive_bandage: alterado REGex para evitar informações desnecessárias

--- src/main/java/factories/AlterTableQueryFactory.java
+++ src/main/java/factories/AlterTableQueryFactory.java
@@ -15,7 +15,7 @@ public class AlterTableQueryFactory implements QueryFactory {
 
 	@Override
 	public Query createQuery(String statement) {
-		String regex = "^\\s*ALTER\\s*TABLE\\s*(\\w*)\\s*(\\w*)\\s*(\\w*)\\s*(\\w*)";
+		String regex = "ALTER\\s*TABLE\\s*(\\w*)\\s*(\\w*)\\s*(\\w*)\\s*(\\w*)";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(statement);
 
